d423959bff7151953083ffb9987facc7acfeed28
Shubhanshu <148634340+kumarshubhh@users.noreply.github.com>
2024-10-22 08:31:03 +0000

    set homepage for github pages deploy

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "SHUBHANSHU",
+  title: "Web Designer & Frontend Developer",
+  email: "subhanshukumar290@gmail.com",
+  gitHub: "kumarshubhh",
+  instagram: "",
+  linkedIn: "shubhanshu-kumar",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "subhanshu kumar",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,10 +31,10 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Mini Project (Spotify Clone) 🎉",
     description:
       "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+    url: "https://github.com/kumarshubhh/Spotify-Clone.git",
   },
   {
     title: "Web Development for Beginners",
@@ -46,14 +46,10 @@ const projectList = [
     title: "My Resume Site",
     description:
       "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+    url: "https://github.com/kumarshubhh/resume.git",
   },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
-  },
+  
+    
 ];
 
 const Portfolio = () => {
